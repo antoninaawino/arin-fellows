6d91bc274d70182189fde94daf1f655583d71e0b
Antonina Awino <antoninaawino@gmail.com>
2026-03-24 12:15:02 +0300

    AI Fellows

---
+++ data/fellows/ai-fellows.js
@@ -0,0 +1,27 @@
+export const aiFellows = [
+  {
+    id: "ai1",
+    name: "Benjamin Aganze Marhegane",
+    image: "/img/ai-fellows/Aganze Marhegane Benjamin.webp ",
+    title:
+      "Teacher – Researcher",
+    bio: "Benjamin Aganze Marhegane is a young Congolese researcher from the Democratic Republic of the Congo. He is a teacher at the Institut Supérieur de Développement Rural de Bukavu (ISDR-Bukavu) and a researcher at the Laboratoire d’Analyses et de Recherches sur les Dynamiques Économiques et Sociales (LARDES) at the University of Parakou in Benin. He is currently pursuing his studies in Urban and Regional Planning at Necmettin Erbakan University (NEÜ) in Konya, Turkey.Interested in the contemporary dynamics characterizing African cities, notably the rapid growth of the urban population, accelerated and often informal urbanization, governance challenges in urban areas, and the effects of climate change, his research focuses mainly on urban and regional planning, environmental governance, and climate resilience strategies in African cities.",
+    category: "AI",
+  },
+    {
+    id: "ai2",
+    name: "Abdel Nasser Yahaya",
+    image: "/img/ai-fellows/Abdel Nasser Yahaya.webp ",
+    title: " ",
+    bio: "coming soon",
+    category: "AI",
+  },
+    {
+    id: "ai3",
+    name: "Afuye Gbenga",
+    image: "/img/ai-fellows/Afuye Gbenga.webp ",
+    title: " ",
+    bio: "coming soon",
+    category: "AI",
+  },
+];
